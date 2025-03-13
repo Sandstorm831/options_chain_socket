be1ddaa6633ce152b0f5dd1dade9fed6ef3bee8f
Sandstorm831 <gargrohan138@gmail.com>
2025-03-13 20:52:00 +0530

    added approximate options pricing and actual percentage changes with correct seed initialisation of option prices

--- src/socket.ts
+++ src/socket.ts
@@ -14,23 +14,101 @@ function randomInAB(miner: number, maxer: number) {
   return Math.random() * (maxer - miner) + miner;
 }
 
-function dataBuilder() {
-  let a = [];
+function seedPopulator() {
   for (let i = 0; i < 75; i++) {
     const x = [];
-    x.push(randomInAB(1, 10));
-    x.push(randomInAB(20000, 25000));
-    x.push(randomInAB(20000, 25000));
-    x.push(randomInAB(20000, 25000));
-    x.push(randomInAB(1, 10));
-    a.push(x);
+    let callP: number, putP: number;
+    if (underlying > initialStrike + i * 100) {
+      callP = underlying - initialStrike - i * 100;
+      putP = Number(randomInAB(0, 5).toFixed(2));
+    } else {
+      callP = Number(randomInAB(0, 5).toFixed(2));
+      putP = initialStrike + i * 100 - underlying;
+    }
+    x.push(callP);
+    x.push(putP);
+    seed.push(x);
   }
-  return a;
 }
 
+function dataBuilder() {
+  let a = [];
+  if (preCalcData.length === 0) {
+    for (let i = 0; i < 75; i++) {
+      const x = [];
+      const callP =
+        seed[i][0] + Number(randomInAB(-5, 5).toFixed(2)) * seed[i][0];
+      const callCP = Number(
+        (((callP - seed[i][0]) / seed[i][0]) * 100).toFixed(2),
+      );
+      const putP =
+        seed[i][1] + Number(randomInAB(-5, 5).toFixed(2)) * seed[i][1];
+      const putCP = Number(
+        (((seed[i][1] - putP) / seed[i][1]) * 100).toFixed(2),
+      );
+      x.push(callCP);
+      x.push(callP);
+      x.push(initialStrike + i * 100);
+      x.push(putP);
+      x.push(putCP);
+      a.push(x);
+    }
+    return a;
+  } else {
+    for (let i = 0; i < 75; i++) {
+      const x = [];
+      const probC = Number(Math.random().toFixed());
+      const probP = Number(Math.random().toFixed(2));
+      if (probC < 0.3) {
+        const callP =
+          seed[i][0] + Number(randomInAB(-5, 5).toFixed(2)) * seed[i][0];
+        const callCP = Number(
+          (((callP - preCalcData[i][1]) / preCalcData[i][1]) * 100).toFixed(2),
+        );
+        x.push(callCP);
+        x.push(callP);
+        x.push(initialStrike + i * 100);
+      } else {
+        x.push(preCalcData[i][0]);
+        x.push(preCalcData[i][1]);
+        x.push(initialStrike + i * 100);
+      }
+      if (probP < 0.3) {
+        const putP =
+          seed[i][1] + Number(randomInAB(-5, 5).toFixed(2)) * seed[i][1];
+        const putCP = Number(
+          (((preCalcData[i][3] - putP) / preCalcData[i][3]) * 100).toFixed(2),
+        );
+        x.push(putP);
+        x.push(putCP);
+      } else {
+        x.push(preCalcData[i][3]);
+        x.push(preCalcData[i][4]);
+      }
+      a.push(x);
+    }
+    return a;
+  }
+  // for (let i = 0; i < 75; i++) {
+  //   const x = [];
+  //   x.push(Number(randomInAB(1, 10).toFixed(2)));
+  //   x.push(Number(randomInAB(20000, 25000).toFixed(2)));
+  //   x.push(Number((initialStrike + i * 100).toFixed(2)));
+  //   x.push(Number(randomInAB(20000, 25000).toFixed(2)));
+  //   x.push(Number(randomInAB(1, 10).toFixed(2)));
+  //   a.push(x);
+  // }
+  // return a;
+}
+
+const initialStrike = 18000;
+let seed: number[][] = [];
+let underlying: number = 22498;
 let connections = 0;
 let timeInterval: NodeJS.Timeout | null = null;
-let preCalcData = dataBuilder();
+let preCalcData: number[][] = dataBuilder();
+
+seedPopulator();
 
 io.on("connection", (socket) => {
   connections += 1;
