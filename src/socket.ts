d65dfbe4397950bc5c7ec828f14850d947b2ec3d
Sandstorm831 <gargrohan138@gmail.com>
2025-03-13 12:32:09 +0530

    completed basic socket server setup

--- src/socket.ts
+++ src/socket.ts
@@ -10,8 +10,51 @@ const io = new Server(httpServer, {
   },
 });
 
+function randomInAB(miner: number, maxer: number) {
+  return Math.random() * (maxer - miner) + miner;
+}
+
+function dataBuilder() {
+  let a = [];
+  for (let i = 0; i < 75; i++) {
+    const x = [];
+    x.push(randomInAB(1, 10));
+    x.push(randomInAB(20000, 25000));
+    x.push(randomInAB(20000, 25000));
+    x.push(randomInAB(20000, 25000));
+    x.push(randomInAB(1, 10));
+    a.push(x);
+  }
+  return a;
+}
+
+let connections = 0;
+let timeInterval: NodeJS.Timeout | null = null;
+let preCalcData = dataBuilder();
+
 io.on("connection", (socket) => {
-  // ...
+  connections += 1;
+  if (connections > 0 && timeInterval === null) {
+    console.log("starting data transmission");
+    io.emit("data", preCalcData);
+    preCalcData = dataBuilder();
+    timeInterval = setInterval(() => {
+      io.emit("data", dataBuilder());
+      preCalcData = dataBuilder();
+    }, 1000);
+  }
+  socket.on("disconnect", () => {
+    connections -= 1;
+    if (connections <= 0) {
+      if (timeInterval) {
+        clearInterval(timeInterval);
+        timeInterval = null;
+      }
+      console.log("Stopping transmission");
+    }
+  });
 });
 
-httpServer.listen(8080);
+httpServer.listen(8080, () => {
+  console.log("Server is listening at http://localhost:8080");
+});
